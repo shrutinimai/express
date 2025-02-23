20ff198823b129722bfeb4ba2c74461b7ac0a442
shrutinimai <nimaishruti@gmail.com>
2025-02-23 22:49:09 +0530

    intro to middleware ( logging the data to the terminal)

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-const http=require('http');
-const express=require('express');
-const app=express();
-app.use((req,res,next)=>{
-    console.log('in the middleware');
-    next();
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/add-product", (req, res) => {
+    res.send(`
+        <form action="/add-product" method="POST">
+            <label>Product Name:</label>
+            <input type="text" name="productName" required />
+            <br />
+            <label>Size:</label>
+            <input type="text" name="size" required />
+            <br />
+            <button type="submit">Add Product</button>
+        </form>
+    `);
 });
-app.use((req,res,next)=>{
-    console.log('in the another middleware');
-    res.send('<h1>hello from express!</h1>')
-})
-const server=http.createServer(app);
-server.listen(3000)
+
+app.post("/add-product", (req, res) => {
+    console.log("Form Data:", req.body); 
+    res.send("Product added successfully!");
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
